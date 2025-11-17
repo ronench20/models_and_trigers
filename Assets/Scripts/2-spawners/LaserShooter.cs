bd4930c78c3dd0907a18b41784e675d184e5f2d2
ronench20 <ronencher@gmail.com>
2025-11-17 14:18:35 +0200

    first commit

---
+++ Assets/Scripts/2-spawners/LaserShooter.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * This component spawns the given laser-prefab whenever the player clicks a given key.
+ * It also updates the "scoreText" field of the new laser.
+ */
+public class LaserShooter: ClickSpawner {
+    [SerializeField]
+    [Tooltip("How many points to add to the shooter, if the laser hits its target")]
+    int pointsToAdd = 1;
+    [SerializeField] Text ammoText;
+    [SerializeField] int ammo;
+    [SerializeField] float cooldown = 0.5f;
+
+    // A reference to the field that holds the score that has to be updated when the laser hits its target.
+    private NumberField scoreField;  
+
+    private void Start() {
+        scoreField = GetComponentInChildren<NumberField>();
+        if (!scoreField)
+            Debug.LogError($"No child of {gameObject.name} has a NumberField component!");
+    }
+
+    private void AddScore()
+    {
+        scoreField.AddNumber(pointsToAdd);
+    }
+
+    protected override GameObject spawnObject() {
+        
+        if (timer < cooldown)
+        {
+            return null;
+        }
+        timer = 0f;
+        
+        if (ammo > 0)
+        {
+            ammo--;
+            ammoText.text = ammo.ToString();
+        }
+        else
+        {
+            return null;
+        }
+
+        
+        GameObject newObject = base.spawnObject();  // base = super
+        DestroyOnTrigger2D newObjectDestroyer = newObject.GetComponent<DestroyOnTrigger2D>();
+        if (newObjectDestroyer)
+            newObjectDestroyer.onHit += AddScore;
+        return newObject;
+    }
+}
---
+++ Assets/Scripts/4-levels/NumberField.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/**
+ * This component should be attached to a TextMeshPro object.
+ * It allows to feed an integer number to the text field.
+ */
+[RequireComponent(typeof(TextMeshPro))]
+public class NumberField : MonoBehaviour {
+    private int number;
+    private TextMeshPro textField;
+    [SerializeField] Text ststicScoreText;
+    
+    private void Awake() {
+        textField = GetComponent<TextMeshPro>();
+    }
+
+    public int GetNumber() {
+        return this.number;
+    }
+
+    public void SetNumber(int newNumber) {
+        this.number = newNumber;
+        GetComponent<TextMeshPro>().text = newNumber.ToString();
+        textField.text = newNumber.ToString();
+        GAME_STATUS.playerScore = newNumber;
+        ststicScoreText.text = newNumber.ToString();
+    }
+
+    public void AddNumber(int toAdd) {
+        SetNumber(this.number + toAdd);
+    }
+}
